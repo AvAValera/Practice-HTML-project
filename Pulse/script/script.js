6c9f79b1e4da0f79b9777f22bde3762f03dcaf4e
AvAValera <avavarenik07@gmail.com>
2021-02-28 22:05:34 +0200

    add slider edit adaptability

---
+++ Pulse/script/script.js
@@ -0,0 +1,8 @@
+$(document).ready(function(){
+    $('.carousel__inner').slick({
+        speed: 1200,
+        prevArrow: '<button type="button" class="slick-prev"><img src="img/slide/left.png" alt=""></button>',
+        nextArrow: '<button type="button" class="slick-next"><img src="img/slide/right.png" alt=""></button>',
+
+    });
+});
