fa27110b719b921f39fdd96f553eae86573f1af5
AvAValera <avavarenik07@gmail.com>
2021-03-12 21:27:57 +0200

    edit main menu, edit script, correct block portfolio

--- Portfolio/script/script.js
+++ Portfolio/script/script.js
@@ -1,12 +1,17 @@
 const gamburger = document.querySelector('.hamburger'),
     menu = document.querySelector('.menu__block'),
-    close = document.querySelector('.menu__close');
+    close = document.querySelector('.menu__close'),
+    overlay = document.querySelector('.menu__overlay');
 
 gamburger.addEventListener('click', () =>{
     menu.classList.add('active')
+    overlay.classList.add('active')
+    document.body.style.overflow = 'hidden';
 });
 close.addEventListener('click',() =>{
     menu.classList.remove('active')
+    overlay.classList.remove('active')
+    document.body.style.overflow = 'visible';
 });
 
 const percent = document.querySelectorAll('.skills__graph-percent'),
