285a9f4f6f84e6ae380bfe86ac976263a09b62bd
AvAValera <avavarenik07@gmail.com>
2021-03-12 14:56:15 +0200

    finish skills block, finish portfolio block, finish price block

--- Portfolio/script/script.js
+++ Portfolio/script/script.js
@@ -1,10 +1,17 @@
-const gamburger = document.querySelector('.hamburger')
-const menu = document.querySelector('.menu__block')
-const close = document.querySelector('.menu__close')
+const gamburger = document.querySelector('.hamburger'),
+    menu = document.querySelector('.menu__block'),
+    close = document.querySelector('.menu__close');
 
 gamburger.addEventListener('click', () =>{
     menu.classList.add('active')
 });
 close.addEventListener('click',() =>{
     menu.classList.remove('active')
+});
+
+const percent = document.querySelectorAll('.skills__graph-percent'),
+    line = document.querySelectorAll('.skills__graph-line_orange');
+
+percent.forEach((el, i) =>{
+    line[i].style.width = el.innerHTML;
 });
