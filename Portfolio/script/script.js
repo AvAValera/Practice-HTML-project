8bddd63fb705d5118151bfaeee40b51eb9dbbdc9
AvAValera <avavarenik07@gmail.com>
2021-03-12 10:20:28 +0200

    edit experience block, add skills block

--- Portfolio/script/script.js
+++ Portfolio/script/script.js
@@ -4,8 +4,6 @@ const close = document.querySelector('.menu__close')
 
 gamburger.addEventListener('click', () =>{
     menu.classList.add('active')
-    console.log('click');
-    
 });
 close.addEventListener('click',() =>{
     menu.classList.remove('active')
